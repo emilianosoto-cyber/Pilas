bbeb77c3f9e99b511613d343fabc1dc4b76708a8
Emiliano Soto <emiliano.soto@uabc.edu.mx>
2026-02-13 23:07:28 -0800

    Se genera la pila de manera muy sencilla.

--- src/main/java/org/example/Pila.java
+++ src/main/java/org/example/Pila.java
@@ -1,4 +1,32 @@
 package org.example;
+import java.util.ArrayList;
 
 public class Pila {
+    private ArrayList<Object> datos = new ArrayList<>();
+
+    public void push(Object valor) {
+        datos.add(valor);
+    }
+
+    public Object pop() {
+        if (isEmpty()) {
+            return null;
+        }
+        return datos.remove(datos.size() - 1);
+    }
+
+    public Object peek() {
+        if (isEmpty()) {
+            return null;
+        }
+        return datos.get(datos.size() - 1);
+    }
+
+    public boolean isEmpty() {
+        return datos.isEmpty();
+    }
+
+    public int size() {
+        return datos.size();
+    }
 }
