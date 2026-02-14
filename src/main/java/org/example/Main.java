d6ef53ed975f5607f0eb906fa722f2dc3d752de7
Emiliano Soto <emiliano.soto@uabc.edu.mx>
2026-02-13 23:13:49 -0800

    Modificamos clase Main.java para probar Pila, invierteCadena, revisarSintaxis y ordenarAscendente

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,36 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Pila pilaStrings = new Pila();
+        pilaStrings.push("uno");
+        pilaStrings.push("dos");
+        pilaStrings.push("tres");
+        System.out.println("Tamaño: " + pilaStrings.size());
+        System.out.println("Cima: " + pilaStrings.peek());
+        System.out.println("Pop: " + pilaStrings.pop());
+        System.out.println("Cima ahora: " + pilaStrings.peek());
+
+        String original = "Hola mundo";
+        String invertida = EjerciciosPila.invierteCadena(original);
+        System.out.println("Original: " + original);
+        System.out.println("Invertida: " + invertida);
+
+        String s1 = "{[()]}";
+        String s2 = "{[(]}]";
+        String s3 = "{ [ ( ] } ]";
+        System.out.println("revisarSintaxis(s1) = " + EjerciciosPila.revisarSintaxis(s1));
+        System.out.println("revisarSintaxis(s2) = " + EjerciciosPila.revisarSintaxis(s2));
+        System.out.println("revisarSintaxis(s3) = " + EjerciciosPila.revisarSintaxis(s3));
+
+        int[] datos = {5, 1, 4, 2, 9, 3};
+        Pila pilaOrdenada = EjerciciosPila.ordenarAscendente(datos);
+
+        System.out.println("Sacar elementos en orden (menor a mayor):");
+        while (!pilaOrdenada.isEmpty()) {
+            System.out.print(pilaOrdenada.pop() + " ");
         }
+        System.out.println();
     }
 }
