2582a22d16db22632546a73eb86f827fdc59b453
Emiliano Soto <emiliano.soto@uabc.edu.mx>
2026-02-13 23:10:25 -0800

    Agregamos método revisarSintaxis para (), [], {} usando Pila

--- src/main/java/org/example/EjerciciosPila.java
+++ src/main/java/org/example/EjerciciosPila.java
@@ -6,19 +6,43 @@ public class EjerciciosPila {
         if (cadena == null) {
             return "";
         }
-
         Pila pila = new Pila();
         for (int i = 0; i < cadena.length(); i++) {
             char c = cadena.charAt(i);
             pila.push(c);
         }
-
         String resultado = "";
         while (!pila.isEmpty()) {
             Character c = (Character) pila.pop();
             resultado = resultado + c;
         }
-
         return resultado;
+    }
+
+    public static boolean revisarSintaxis(String cadena) {
+        if (cadena == null) {
+            return false;
+        }
+
+        Pila pila = new Pila();
+
+        for (int i = 0; i < cadena.length(); i++) {
+            char c = cadena.charAt(i);
+
+            if (c == '(' || c == '[' || c == '{') {
+                pila.push(c);
+            } else if (c == ')' || c == ']' || c == '}') {
+                if (pila.isEmpty()) {
+                    return false;
+                }
+                char arriba = (Character) pila.pop();
+
+                if (arriba == '(' && c != ')') return false;
+                if (arriba == '[' && c != ']') return false;
+                if (arriba == '{' && c != '}') return false;
+            }
+        }
+
+        return pila.isEmpty();
     }
 }
